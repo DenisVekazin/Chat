b1c04a22c15ed57a80b994c174c4f547ecb7baa5
LaDante <baev.vladislav@cosysoft.ru>
2023-10-04 00:15:11 +0400

    template for Fidget

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import logo from './Images/ErenYeager.jpg';
 import './App.css';
 
 function App() {
+    const list = [
+        { title: 'Эрен Йегер', description: 'Главный герой', img: logo },
+        { title: 'Эрен Йегер 2', description: 'Главный герой 2', img: logo },
+        { title: 'Эрен Йегер 3', description: 'Главный герой 3', img: logo }
+    ]
+
   return (
-    <div className="card">
-  <img src={logo} alt="Avatar" style={{width: '100%'}}/>
-  <div class="container">
-    <h4><b>Эрен Йегер</b></h4>
-    <p>Главный герой</p>
-  </div>
-  </div>
+      <div className="root-container">
+          <Card title={'Эрен Йегер'} description={'Главный герой'} img={logo} />
+      </div>
   );
+}
+
+const Card = ({ title, description, img }) => {
+    return (
+        <div className="card">
+            <img src={img} alt={`Avatar-${title}`} style={{ width: '100%' }}/>
+            <div className="container">
+                <h4><b>{title}</b></h4>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
 }
 
 export default App;
